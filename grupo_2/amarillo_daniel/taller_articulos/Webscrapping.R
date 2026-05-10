b36ed5fe80ea0c200aebeacf701b54551cfac7e1
Damarillo <102869725+Damarillo@users.noreply.github.com>
2026-05-09 19:51:50 -0500

    Webscraping terminado

--- grupo_2/amarillo_daniel/taller_articulos/Webscrapping.R
+++ grupo_2/amarillo_daniel/taller_articulos/Webscrapping.R
@@ -1,6 +1,6 @@
 paquetes <- c(
-  "rvest", "xml2", "httr2", "dplyr", "stringr",
-  "purrr", "tibble", "janitor", "readr", "knitr"
+  "rvest", "xml2", "httr2", "dplyr", "stringr","openalexR",
+  "purrr", "tibble", "janitor", "readr", "knitr", "tidyr"
 )
 
 # Verificamos qué paquetes faltan
@@ -44,11 +44,7 @@ nodo <- respuesta |>
   resp_body_html()
 nodo
 }
-
-#TITULO
-#################
-
-extraccion_general <- function(nodo) {
+extraer_general <- function(nodo) {
   # Extraemos el títulos de los articulos
   titulo <- nodo |>
     html_elements("[data-testid='block-primitivetitle']") |>
@@ -60,6 +56,7 @@ extraccion_general <- function(nodo) {
   doi <- nodo |> 
     html_elements( " a,[target='_blank'], .c-Button--link") |>
     html_text()
+  #filtramos por links que contengan https doi 
   doi <- doi[grep(pattern="https://doi*",doi)]
   #Creamos una tabla con la información general de cada journal 
   tibble(
@@ -69,59 +66,145 @@ extraccion_general <- function(nodo) {
   )
 }
 
-
-generalities <- data.frame()
+papers <- data.frame()
 for (i in issues_url) {
   nodo = crear_nodo(i)
-  df <- extraccion_general(nodo)
-  generalities <- rbind(generalities,df)
+  df <- extraer_general(nodo)
+  papers <- rbind(papers,df)
 }
 
-doi_prueba <- generalities$doi[1]
-pagina <- crear_nodo(doi_prueba)
 
+extraer_issue <- function(url){
+ 
+  nodo <- crear_nodo(url)
  #EXTRACCIÓN DOI (LLAVE EN COMUN CON GENERALITIES)
-doi <- pagina |> 
+doi <- nodo |> 
   html_elements( " a,[target='_blank'], .c-Button--link") |>
   html_text()
 doi <- doi[grep(pattern="https://doi*",doi)][1]
 
  #AUTORES 
-autores_ex <- pagina |> 
+autores_ex <- nodo |> 
   html_elements("[data-testid='author-name']") |>
   html_text()
 autores <- paste(autores_ex,collapse = ",")
- 
  #ABSTRACT
- raw_abstract <- pagina |> html_elements(".abstract.border-bottom.border-bottom-solid.border-bottom-medium") |>
+ raw_abstract <- nodo |> html_elements(".abstract.border-bottom.border-bottom-solid.border-bottom-medium") |>
    html_text2()
- a<- unlist(strsplit(raw_abstract, split = "\n"))
- Backround_and_aims <- a[4]
- Methods <- a[8]
- Results <- a[12]
- Conclusion <- a[16]
+ a <- unlist(strsplit(raw_abstract, split = "\n"))
+ Backround_and_aims <- NA
+ Methods <- NA
+ Results <- NA
+ Conclusion <- NA
+ abstract <- NA
+ if (length(a)<4) {
+   abstract <- a[3] 
+ }else {
+   Backround_and_aims <- a[4]
+   Methods <- a[8]
+   Results <- a[12]
+   Conclusion <- a[16]
+ }
 
- fecha <- pagina |> 
-   html_elements(".printpubdate.c-List__items") |>
+ #FECHA DE PUBLICACIÓN
+ fecha <- nodo |> 
+   html_elements(".onlinepubdate.c-List__items") |>
    html_text2() |>
-   str_remove_all("\t|\n") |>
+   str_squish() |>
    str_remove("^[^0-9]+")
  
+ #Métrica
+ fwci <- oa_fetch(entity = "works", doi = doi)$fwci
+ 
  #URL 
- URL <- pagina |> 
+ URL <- nodo |> 
    html_elements('[rel="canonical"], href')|>
    html_attr("href")
  
- ref_ex <- pagina |> 
+ #REFERENCIAS
+ ref_ex <- NA
+ ref_ex <- nodo |> 
    html_elements(".citationText.text-body1") |>
    html_text()
- #SEPARADOR PROPIO /--/
- referencias <- paste(ref_ex,collapse = "/--/")
  
  
  # CITAS
  dyn_cit = read_html_live(URL)
- dyn_cit$click("[data-tab-id='citedBy-30734']")
- dyn_cit |> html_elements("#citedByWidget") |>
-   html_children("p")
- on.exit(dyn_cit$session$close())
+ try(dyn_cit$click("[data-tab-id='citedBy-30734']"),silent = TRUE)
+ n_citaciones <- NA
+ intentos <- 0
+ while (intentos < 10) {
+   Sys.sleep(3)
+   p <- dyn_cit |> 
+     html_elements("#citedByWidget") |>
+     html_children()
+   
+   texto <- p[2] |> html_text() |> str_squish()
+   
+   if (length(texto) > 0 && nchar(texto) > 0) {
+     n_citaciones <- word(texto, -2)
+     break
+   }
+   intentos <- intentos + 1
+ }
+
+
+  paper_row <- tibble (doi = doi,
+                      url = URL,
+                      fecha_publicacion = fecha,
+                      nro_de_citas = n_citaciones,
+                      fwci = fwci)
+
+ abstract_row <- tibble( doi = doi,
+         resumen = abstract,
+         Backround = Backround_and_aims,
+         Metodos = Methods,
+         Resultados = Results,
+         Conclusion = Conclusion
+         )
+ autores_row <- tibble( doi = doi,
+                        autores = list(autores_ex) )
+ ref_row <- tibble(doi = doi,
+                   list(ref_ex))
+ 
+ dyn_cit$session$close()
+ 
+
+ return(list(paper_row,abstract_row,autores_row,ref_row))
+}
+abstract <- tibble()
+autores <- tibble()
+referencias <- tibble()
+comp_papers <- tibble()
+
+ # for (i in papers$doi){
+ #   tibbles <- extraer_issue(i)
+ #   comp_papers <- comp_papers |> bind_rows(tibbles[1])  
+ #   abstract <- abstract |> bind_rows(tibbles[2]) 
+ #   autores <- autores |> bind_rows(tibbles[3]) 
+ #   referencias <- referencias |> bind_rows(tibbles[4]) 
+ #   
+ # }
+
+
+
+# Ordenamos y separamos las tablas de referencias y autores
+paper_references <- referencias |> tidyr::unnest(`list(ref_ex)`) |> rename(referencia = `list(ref_ex)`)
+paper_references$id_referencia <- as.numeric(as.factor(paper_references$referencia))
+references <- paper_references[c("id_referencia","referencia")] |> unique() |> arrange(id_referencia)
+paper_references <- paper_references |> select(-referencia) |> unique()
+
+
+paper_authors <- autores |> tidyr::unnest(autores) 
+paper_authors$id_autor <- as.numeric(as.factor(paper_authors$autores)) |> unique()
+references <- paper_authors[c("id_autor","autores")] |> unique() |> arrange(id_autor)
+
+papers["journal_name"] <- "Journal of Behavioral Addictions" 
+papers <- papers |> full_join(comp_papers,papers,by="doi")
+papers <- papers |> full_join(paper_authors |>
+                                     group_by(doi) |>
+                                     summarise(n_autores = n()),by="doi")
+
+papers <- papers |> full_join(paper_references |>
+                                     group_by(doi) |>
+                                     summarise(n_referencia = n()),by="doi")
